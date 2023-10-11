b61d4d5531fe24efe093a2040cfe4ab5aa4a811c
BullJohnson <bulljohnson44@gmail.com>
2023-10-10 23:10:37 -0400

    Initial commit.  Added files via upload, (continued)
    Initial commit.  Added files via upload, because github would not allow me to create a new repository, for the downloaded files.

---
+++ test_scores.js
@@ -0,0 +1,100 @@
+
+//arrays could also be initialized empty, to start clean
+let namesArr = ['Ben', 'Joel', 'Judy', 'Anne'];
+let scoresArr = [88, 98, 77, 88];
+
+function getAvgScore() {
+    let i = 0,
+        sum = 0,
+        len = scoresArr.length;
+    let name = '';
+    for (i; i < len; i++) {
+        sum += scoresArr[i];
+    }
+    return sum / len;
+}
+
+function getHighScore() {
+    let i = 0,
+        max = 0,
+        len = scoresArr.length;
+    let name = '';
+    for (i; i < len; i++) {
+        if (scoresArr[i] > max) {
+            max = scoresArr[i];
+            name = namesArr[i];
+        }
+    }
+    return name + ' with score of ' + max;
+}
+
+function initializeResults() {
+    let results = $('#results');
+    let high = getHighScore();
+    let avg = getAvgScore().toFixed(1);
+    $('#highScore').html(high);
+    $('#avgScore').html(avg);
+}
+
+
+function displayScores() {
+    let scores = $("#scores");
+    scores.toggle();
+    $("#name").focus();
+}
+
+function displayResults() {
+    let results = $('#results');
+    results.toggle();
+    $("#name").focus();
+}
+
+function insertNewTableElement(newName, newScore) {
+   $("#name").focus();
+}
+
+function initializeScoresTable() {
+$("#scores_table tr").slice(1).remove();
+for (let i = 0; i < scoresArr.length; i++) {
+    insertNewTableElement($("#scores_table tr:last").after("<tr><td>" + 
+    namesArr[i] + "</td><td>" + scoresArr[i] + "</td></tr>"));
+    }
+}
+
+
+function addScore() {
+    let score = $('#score');
+    let name = $('#name');
+    if (score.val() === '' || name.val() === '') {
+        alert('Name and score must have values');
+        return;
+    }
+    scoresArr.push(parseInt(score.val()));
+    namesArr.push($("#name").val());
+    initializeScoresTable();
+    score.val('');
+    name.val('');
+    initializeResults();
+    $('#scores').show();
+    $('#results').show();
+}
+
+window.onload = function () {
+    $('#display_results').on('click',  function() {
+        displayResults();
+    });
+
+    $('#display_scores').on('click',  function() {
+        displayScores();
+    });
+    $('#add').on('click',  function() {
+        addScore();
+    });
+
+    let name = $('#name');
+    let score = $('#score');
+
+    name.focus();
+    initializeResults();
+    initializeScoresTable();
+}
